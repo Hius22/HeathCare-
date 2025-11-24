e6b3f279b17a035330858c189fef0f86d3a78813
Hius <nguyenhuuhieu02022004@gmail.com>
2025-11-25 03:48:26 +0700

    Upload source

--- src/server.js
+++ src/server.js
@@ -15,8 +15,11 @@ app.use(cors({ origin: true }));
 //     credentials: true                // cho phép gửi cookies / token
 //   }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//config app
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
--- src/services/userService.js
+++ src/services/userService.js
@@ -128,9 +128,9 @@ let createNewUser = (data) => {
                     address: data.address,
                     phonenumber: data.phonenumber,
                     gender: data.gender,
-                    image: data.image,
                     roleId: data.roleId,
-                    positionId: data.positionId
+                    positionId: data.positionId,
+                    image: data.avatar
                 })
 
                 resolve({
@@ -170,7 +170,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: 'Missing required parameters'
@@ -184,13 +184,16 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber;
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
 
                 await user.save();
-                // await db.User.save({
-                //     firstName: data.firstName,
-                //     lastName: data.lastName,
-                //     address: data.address,
-                // });
+
                 resolve({
                     errCode: 0,
                     message: 'Update the user succeeds!'
